25ede93a1e7862767eb69900dacee4d194403062
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-01-27 00:02:20 +0900

    Json送ってもらうところまで

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func Search() {
+
+
+const (
+	ENDPOINT = "https://ja.wikipedia.org/w/api.php?action=query&prop=extracts&titles=%e6%97%a5%e6%9c%ac%e3%81%ae%e8%a8%98%e5%bf%b5%e6%97%a5%e4%b8%80%e8%a6%a7&format=json"
+)
+
+func Search(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	response, _ := http.Get(ENDPOINT) // Endpointにgetを投げる
+	defer response.Body.Close() //deferで終了時に実行
+
+	body, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Fprintln(w, body)
 
 }
 
@@ -14,5 +28,4 @@ func main() {
 	router := httprouter.New()
 	router.GET("/", Search)
 	log.Fatal(http.ListenAndServe(":8080", router))
-
 }
